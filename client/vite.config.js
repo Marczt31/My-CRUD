695511d34f5d2c2483dd9dd1692b9048d1bbcca6
Marc <marczt2002@gmail.com>
2025-05-14 18:41:40 +0200

    chore: files configured

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,23 @@
+// client/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  root: path.resolve(__dirname),
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  build: {
+    outDir: path.resolve(__dirname, '../dist'), 
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:5000',
+    },
+  },
 })
